95a4df90a4fe3aa0631c2412f695bb6f2cf82d7e
Aftab Baig <aftabaig82@gmail.com>
2023-11-14 18:30:43 +0500

    fix ui issue on mobile screen

--- src/modules/player/components/SplashScreen.tsx
+++ src/modules/player/components/SplashScreen.tsx
@@ -9,17 +9,13 @@ export const SplashScreen = () => {
         <h2 className={styles.welcomeText}>{labels.welcomeToSkoopSignage}</h2>
         <div className={styles.inlineDisplay}>
           <h3 className="textLeft">{labels.nextSteps}</h3>
-          <div className={styles.flex24}>
+          <div className={styles.flexGrid24}>
             <h4 className={styles.step}>{labels.step1}</h4>
-            <h4 style={{ paddingLeft: 10, paddingTop: 8 }}>
-              {labels.goTopPlaylistPage}
-            </h4>
+            <h4 className={styles.textPadding}>{labels.goTopPlaylistPage}</h4>
           </div>
-          <div className={styles.flex24}>
+          <div className={styles.flexGrid24}>
             <h4 style={{ color: "#00b7af", paddingTop: 8 }}>{labels.step2}</h4>
-            <h4 style={{ paddingLeft: 10, paddingTop: 8 }}>
-              {labels.goToScreenPage}
-            </h4>
+            <h4 className={styles.textPadding}>{labels.goToScreenPage}</h4>
           </div>
         </div>
       </div>
