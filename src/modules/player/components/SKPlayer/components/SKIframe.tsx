7944164c598352e3d80e64b04b4c0315109b8cf0
Aftab Baig <aftabaig82@gmail.com>
2023-11-27 18:01:18 +0500

    fixed pagosa and nativesun height issue

--- src/modules/player/components/SKPlayer/components/SKIframe.tsx
+++ src/modules/player/components/SKPlayer/components/SKIframe.tsx
@@ -4,27 +4,24 @@ import styles from "../../../../../../styles/Home.module.css";
 import { SKTransition } from "@playerComponents/SKPlayer/components/index";
 export const SKIframe = (props: PlayerPropsModel) => {
   const { index, playlist, transition } = props;
-  const [contentLoaded, setContentLoaded] = useState(false);
   return (
     <SKTransition transition={transition}>
-      <iframe
-        className={styles.player}
-        title="sample"
-        src={playlist.visibility ? playlist.url : ""}
-        onLoad={() => {
-          setContentLoaded(true);
-        }}
-        scrolling="no"
-        style={{
-          border: 0,
-          overflow: "hidden",
-          overflowX: "hidden",
-          overflowY: "hidden",
-          display: contentLoaded && playlist.visibility ? "" : "none"
-        }}
-        name="myFrame"
-        key={index}
-      />
+      {playlist.visibility && (
+        <iframe
+          className={styles.player}
+          title="sample"
+          src={playlist.visibility ? playlist.url : ""}
+          scrolling="no"
+          style={{
+            border: 0,
+            overflow: "hidden",
+            overflowX: "hidden",
+            overflowY: "hidden"
+          }}
+          name="myFrame"
+          key={index}
+        />
+      )}
     </SKTransition>
   );
 };
