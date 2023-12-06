06e78db1c30fd269c148f2bdc4bdfdea0ae1e650
Aftab Baig <aftabaig82@gmail.com>
2023-12-06 15:46:00 +0500

    fixed iframe black background issue

--- src/modules/player/components/SKPlayer/components/SKIframe.tsx
+++ src/modules/player/components/SKPlayer/components/SKIframe.tsx
@@ -3,24 +3,50 @@ import { PlayerPropsModel } from "@models/playerModel";
 import styles from "../../../../../../styles/Home.module.css";
 import { SKTransition } from "@playerComponents/SKPlayer/components/index";
 import { getQueryParams } from "lib/scoop.repo";
-export const SKIframe = (props: PlayerPropsModel) => {
+
+type SKIframeProps = {
+  index: number;
+  playlistEntry: {
+    url: string;
+  };
+  transition: string;
+  entry: string;
+};
+type Visibility = "hidden" | "visible";
+export const SKIframe = (props: SKIframeProps) => {
   const { index, playlistEntry, transition, entry } = props;
+  const [iframeDisplay, setIframeDisplay] = useState<Visibility | undefined>(
+    "hidden"
+  );
+
   return (
     <SKTransition transition={transition}>
-      <iframe
-        className={styles.player}
-        title="sample"
-        src={`${playlistEntry.url}&refresh=false`}
-        scrolling="no"
+      <div
         style={{
-          border: 0,
-          overflow: "hidden",
-          overflowX: "hidden",
-          overflowY: "hidden"
+          width: "100%",
+          height: "100vh",
+          background: "black"
         }}
-        name="myFrame"
-        key={index}
-      />
+      >
+        <iframe
+          className={styles.player}
+          title="sample"
+          src={`${playlistEntry.url}&refresh=true`}
+          onLoad={() => {
+            setIframeDisplay("visible");
+          }}
+          scrolling="no"
+          style={{
+            border: 0,
+            overflow: "hidden",
+            overflowX: "hidden",
+            overflowY: "hidden",
+            visibility: iframeDisplay
+          }}
+          name="myFrame"
+          key={index}
+        />
+      </div>
     </SKTransition>
   );
 };
